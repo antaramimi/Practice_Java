97ce20f04c312cb14fb307e4464501c47452bd03
Antara <antaramimi@gmail.com>
2018-05-23 12:21:53 +0100

    adding Program

---
+++ src/com/company/ReverseNumber.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+
+    public static void ReverseNumberUserInput(){
+        int num=0;
+        int rev=0;
+
+        Scanner s =new Scanner(System.in);
+        System.out.println("Input your number and press enter: ");
+        num=s.nextInt();
+        while(num!=0){
+            int remainder = num % 10;
+            rev=rev*10 + remainder;
+            num=num/10;
+            }
+        System.out.println("Reverse of input number is:" + rev);
+
+
+    }
+
+
+
+    public static void main (String[]args){
+
+        ReverseNumberUserInput();
+    }
+
+}
